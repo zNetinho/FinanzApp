c578494e34ffdd1842fece6e7fc6d6012384219c
Neto Flavio <95658733+zNetinho@users.noreply.github.com>
2024-07-01 07:58:44 -0300

    feat/Created pop-up for add transactions with react-hook-form and zod

--- src/app/dashboard/_components/CardOverview.tsx
+++ src/app/dashboard/_components/CardOverview.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
@@ -6,9 +7,15 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 import { formatterCurrencyMoney } from '@/lib/formatterCurrency'
+import { cn } from '@/lib/utils'
+import React from 'react'
+import { FormTransactions } from './transactions/FormTransactions'
 
 type Props<T = unknown> = {
   children: React.ReactNode
@@ -49,9 +56,58 @@ function CardOverview({ list }: MockPropsOverview) {
     <Card>
       <HeaderCard className="flex flex-row gap-10 items-center justify-around">
         <TitleCard className="text-base">Overview</TitleCard>
-        <Button variant="outline" className="border">
-          VIEW ALL
-        </Button>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="outline" className="border">
+              VIEW ALL
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-80">
+            <FormTransactions />
+            {/* <div className="grid gap-4">
+              <div className="space-y-2">
+                <h4 className="font-medium leading-none">Dimensions</h4>
+                <p className="text-sm text-muted-foreground">
+                  Set the dimensions for the layer.
+                </p>
+              </div>
+              <div className="grid gap-2">
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Label htmlFor="width">Width</Label>
+                  <Input
+                    id="width"
+                    defaultValue="100%"
+                    className="col-span-2 h-8"
+                  />
+                </div>
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Label htmlFor="maxWidth">Max. width</Label>
+                  <Input
+                    id="maxWidth"
+                    defaultValue="300px"
+                    className="col-span-2 h-8"
+                  />
+                </div>
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Label htmlFor="height">Height</Label>
+                  <Input
+                    id="height"
+                    defaultValue="25px"
+                    className="col-span-2 h-8"
+                  />
+                </div>
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Label htmlFor="maxHeight">Max. height</Label>
+                  <Input
+                    id="maxHeight"
+                    defaultValue="none"
+                    className="col-span-2 h-8"
+                  />
+                </div>
+              </div>
+            </div> */}
+          </PopoverContent>
+        </Popover>
       </HeaderCard>
       <ContentCard className="flex flex-wrap gap-6">
         {list.map((e, index) => (
@@ -67,4 +123,4 @@ function CardOverview({ list }: MockPropsOverview) {
   )
 }
 
-export { CardOverview, HeaderCard, ContentCard, TitleCard, DescriptionCard }
+export { CardOverview, ContentCard, DescriptionCard, HeaderCard, TitleCard }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,3 @@
-import { FooterNavigation } from "@/components/FooterNavigation";
-import Header from "@/components/Header";
-import CardOverview from "@/components/ui/CardOverview";
-import Image from "next/image";
-
-const list = [
-  {
-    category: "Incomes",
-    total_amount: 5234
-  },
-  {
-    category: "Balance",
-    total_amount: 123456
-  },
-  {
-    category: "Expenses",
-    total_amount: 2789
-  },
-  {
-    category: "Savings",
-    total_amount: 3456
-  },
-]
-
 export default function Home() {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div></div>
 }
