3a42e8367b3d1cad1d5f9a5499fd04c9f5b5a12a
Neto Flavio <95658733+zNetinho@users.noreply.github.com>
2024-07-09 07:04:38 -0300

    feat/added social login with github and updated the next-auth lib to v5

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,3 @@
-import NextAuth from 'next-auth'
-import { config } from '../../../../../auth'
+import { handlers } from "../../../../../auth"
 
-const handler = NextAuth(config)
-
-export { handler as GET, handler as POST }
+export const { GET, POST } = handlers
